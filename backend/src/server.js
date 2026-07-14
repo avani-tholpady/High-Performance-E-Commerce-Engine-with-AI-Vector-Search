6542b79ddc1027694ca074aafe8ee5c61af3d156
avani-tholpady <avanitholpady09@gmail.com>
2026-07-14 14:18:52 +0530

    refactor: organize backend configuration and constants

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/db");
+const { DEFAULT_PORT } = require("./constants/constants");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || DEFAULT_PORT;
 
 const startServer = async () => {
   await connectDB();
